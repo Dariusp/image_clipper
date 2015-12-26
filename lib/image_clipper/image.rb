7e2b8f63fe96ea6ff96ca8681ec20c67791a9324
Darius Pėža <darius@onrails.lt>
2015-12-26 21:50:03 +0200

    Update image.rb
    fixed path agai

--- lib/image_clipper/image.rb
+++ lib/image_clipper/image.rb
@@ -27,7 +27,7 @@ module ImageClipper
 				Geometry.new("#{(geometry.width * percent).round}x#{(geometry.height * percent).round}")
 			end
 
-			`convert "#{path}" -resize '#{geo.width}x#{geo.height}' #{new_file_path}`
+			`convert "#{path}" -resize '#{geo.width}x#{geo.height}' "#{new_file_path}"`
 
 			Image.new(new_file_path)
 		end
